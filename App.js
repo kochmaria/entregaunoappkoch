d4395ff667fac86ffb4ac5728b25fe49437aa038
koch <koch@192.168.1.4>
2024-01-24 23:32:04 -0300

    Your commit message here

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mi Aplicacion</Text>
+      <Text>Mi Aplicacion para Android</Text>
+      
+
       <Text></Text>
       <StatusBar style="auto" />
     </View>
